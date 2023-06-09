996626583e2f9f681223de4026d4620107a74645
bruceliu <bruceliu1107@gmail.com>
2023-06-09 13:34:40 -0400

    remove header

--- LoginController.js
+++ LoginController.js
@@ -76,7 +76,6 @@ export default class LoginController extends Component {
           <ScrollView
             contentInsetAdjustmentBehavior="automatic"
             style={styles.scrollView}>
-            <Header />
             {global.HermesInternal == null ? null : (
               <View style={styles.engine}>
                 <Text style={styles.footer}>Engine: Hermes</Text>
